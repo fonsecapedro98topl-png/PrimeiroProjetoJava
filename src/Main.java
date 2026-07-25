256a51433dc223a660705da8d7622378cefa7dce
Pedro F Fonseca <fonseca9813@hotmail.com>
2026-07-24 21:41:47 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Teste Java ===");
+
+        String nome = "Pedro";
+        int idade = 20;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Soma: " + (a + b));
+        System.out.println("Multiplicação: " + (a * b));
+
+        if (a > b) {
+            System.out.println("10 é maior que 5");
+        }
+
+        System.out.println("Fim do programa!");
+    }
+}
